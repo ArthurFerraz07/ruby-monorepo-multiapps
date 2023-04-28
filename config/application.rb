4dcd2a8ecdd4cab46f52df68c4a525c731b869a2
Arthur Ferraz <arthur.ferraz07@hotmail.com>
2023-04-27 23:47:44 -0300

    creates a big rails api monolith

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,10 @@ module AlmostRailsMonorepo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
   end
 end
