18e1728e2fb37b2e83b7149b5848824eb69cdd63
Arthur Ferraz <arthur.ferraz07@hotmail.com>
2023-05-01 20:47:20 -0300

    prices

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
     post 'signin', to: 'identities#signin'
     post 'signup', to: 'identities#signup'
   end
+
+  scope '/prices' do
+    get '/', to: 'prices#index'
+  end
 end
