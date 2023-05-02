5ca3e6afa3dc70263d625a44c907dfec1f35cc19
Arthur Ferraz <arthur.ferraz07@hotmail.com>
2023-05-02 09:16:00 -0300

    remove monolith

--- monolith/app/services/prices_service.rb
+++
@@ -1,7 +0,0 @@
-class PricesService < ApplicationService
-  def call
-    ('a'..'r').to_a.each_with_index.map do |letter, index|
-      { currency: "USD#{letter.upcase}", price: index * 10 + 1 }
-    end
-  end
-end
