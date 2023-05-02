baa6046e28546c39ca89ac6df4b25b9a4adf02e2
Arthur Ferraz <arthur.ferraz07@hotmail.com>
2023-05-02 08:24:11 -0300

    import withdraw files

---
+++ withdraw_app/app/models/withdraw.rb
@@ -0,0 +1,9 @@
+class Withdraw < ApplicationRecord
+  belongs_to :identity
+
+  enum status: {
+    pending: 'pending',
+    processing: 'processing',
+    done: 'done'
+  }
+end
--- withdraw_app/config/routes.rb
+++ withdraw_app/config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  scope 'withdraw' do
+    get '/', to: 'withdraws#index'
+    post '/', to: 'withdraws#start'
+  end
 end
