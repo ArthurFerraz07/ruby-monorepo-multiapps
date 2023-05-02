a94eeadbbb6bc10534c294739cf9f1c081211cbb
Arthur Ferraz <arthur.ferraz07@hotmail.com>
2023-05-02 10:27:47 -0300

    readme and readonly db

--- identity_app/app/models/identity_app/application_record.rb
+++ identity_app/app/models/identity_app/application_record.rb
@@ -2,6 +2,10 @@ module IdentityApp
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
 
-    connects_to database: { writing: :identity_app, reading: :identity_app }
+    if ENV['APP_NAME'] == 'identity_app'
+      connects_to database: { writing: :identity_app, reading: :identity_app }
+    else
+      connects_to database: { reading: :identity_app }
+    end
   end
 end
